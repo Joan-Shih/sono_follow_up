50517f46bc9f4f9b6b7588918d49ad81fbf67cd3
Joan Shih <b0955679421@gmail.com>
2024-05-08 18:35:51 +0800

    finish basic function and layout

--- main.js
+++ main.js
@@ -1,5 +1,48 @@
 $(document).ready(function(){
-    $("button").click(function(){
-      $("#test").hide();
+    $(".sono").hide();
+
+    $("#r1").click(function(){
+      $(".sono").hide();
+      $("#s1").show();
+      $("#title").text("Region 1");
     });
+    $("#r2").click(function(){
+      $(".sono").hide();
+      $("#s2").show();
+      $("#title").text("Region 2");
+    });
+    $("#r3").click(function(){
+      $(".sono").hide();
+      $("#s3").show();
+      $("#title").text("Region 3");
+    });
+    $("#r4").click(function(){
+      $(".sono").hide();
+      $("#s4").show();
+      $("#title").text("Region 4");
+    });
+    $("#r5").click(function(){
+      $(".sono").hide();
+      $("#s5").show();
+      $("#title").text("Region 5");
+    });
+    $("#r6").click(function(){
+      $(".sono").hide();
+      $("#s6").show();
+      $("#title").text("Region 6");
+    });
+
+    $(".region").click(function(){
+      if ($(this).css("background-color") == "rgb(240, 255, 255)"){
+        $(".region").css("background-color", "rgb(240, 255, 255)");
+        $(this).css("background-color", "rgb(255, 0, 0)");
+      }else{
+        $("#title").text("Region");
+        $(".sono").hide();
+        $(".region").css("background-color", "rgb(240, 255, 255)");
+      }
+    });
+
+
+
 });
